d90187d112840371cf157c390e605ba5bb1e9d6b
Anand <anandkumar001k@gmail.com>
2026-05-12 15:30:31 +0530

    made some changes to calculator

--- miniproject/calculator.java
+++ miniproject/calculator.java
@@ -1,55 +1,51 @@
+
 import java.util.Scanner;
-public class calculator{
-    public static void main(String[] args){
+
+public class calculator {
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a : ");
+        System.out.print("Enter value a : ");
         int a = sc.nextInt();
-        System.out.print("Enter b : ");
+        System.out.print("Enter value b : ");
         int b = sc.nextInt();
         System.out.print("Enter the operator(+,-,*,/) : ");
         String operator = sc.next();
 
         int result = 0;
 
-
-        switch(operator){
+        switch (operator) {
             case "+":
-            result = a+b;
-            System.out.print("a+b : "+result);
-            break;
+                result = a + b;
+                System.out.print("a+b : " + result);
+                break;
 
-            case "-": 
-            result = a-b; 
-            System.out.print("a-b : "+result);
-            break;
- 
-            case "*": 
-            result = a*b;
-            System.out.print("a*b : "+result);
-            break;
+            case "-":
+                result = a - b;
+                System.out.print("a-b : " + result);
+                break;
 
-            case "/": 
-            {
-                if(b!=0){
-                    result = a/b;
-                    System.out.println("a/b : "+result);
-                }
-                else{
+            case "*":
+                result = a * b;
+                System.out.print("a*b : " + result);
+                break;
+
+            case "/": {
+                if (b != 0) {
+                    result = a / b;
+                    System.out.println("a/b : " + result);
+                } else {
                     System.out.println("Invalid input");
                 }
-                
+
             }
             break;
-            
+
             default:
                 System.out.println("Invalid operator.");
                 break;
-            
-
 
         }
-
-        
 
     }
 }
